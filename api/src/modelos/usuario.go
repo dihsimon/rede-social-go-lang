6426bb9cb9d2a9cfd71b3d19e1086ac702b42dd5
Diego Simon Carmona <diegosimoncarmona@gmail.com>
2021-05-20 20:02:39 -0300

    Finalizando o cadastro de usuários

--- api/src/modelos/usuario.go
+++ api/src/modelos/usuario.go
@@ -1,6 +1,8 @@
 package modelos
 
 import (
+	"errors"
+	"strings"
 	"time"
 )
 
@@ -12,4 +14,39 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O nome é obrigatorio e não pode estar em branco")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O Nick é obrigatorio e não pode estar em branco")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O E-mail é obrigatorio e não pode estar em branco")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("A Senha é obrigatorio e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
+}
+
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
 }
